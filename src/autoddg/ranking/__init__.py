06581fdc97539c9ace2236a976abb08dab81acad
Beck Zhou <beckzhou712@gmail.com>
2026-07-07 03:37:47 -0400

    Sort ranking package imports

--- src/autoddg/ranking/__init__.py
+++ src/autoddg/ranking/__init__.py
@@ -1,13 +1,12 @@
 from __future__ import annotations
 
-from .qrels import extract_query_rel
-
 from .metrics import (
     compute_avg_single_Q,
     compute_dcg,
     compute_ndcg,
     downstream_task_rank,
 )
+from .qrels import extract_query_rel
 
 __all__ = [
     "compute_avg_single_Q",
